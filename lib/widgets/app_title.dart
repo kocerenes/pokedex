42be441a15e5f7527ecb1c3014092047aa433798
Enes Kocer <kocereness161@gmail.com>
2022-03-20 14:34:17 +0300

    finish app title widget

--- lib/widgets/app_title.dart
+++ lib/widgets/app_title.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pokedex/constants/constants.dart';
+import 'package:pokedex/constants/ui_helper.dart';
 
 class AppTitle extends StatefulWidget {
   const AppTitle({Key? key}) : super(key: key);
@@ -14,22 +15,29 @@ class _AppTitleState extends State<AppTitle> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-            alignment: Alignment.topLeft,
-            child: Text(Constants.title, style: Constants.getTitleTextStyle() )
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Image.asset(
-            pokemonImageUrl,
-            width: 100.w,
-            height: 100.h,
-            fit: BoxFit.fitWidth,
+    return SizedBox(
+      height: UIHelper.getAppTitleWidgetHeight(),
+      child: Stack(
+        children: [
+          Padding(
+            padding: UIHelper.getDefaultPadding(),
+            child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(Constants.title, style: Constants.getTitleTextStyle() )
+            ),
           ),
-        )
-      ],
+          Align(
+            alignment: Alignment.topRight,
+            child: Image.asset(
+              pokemonImageUrl,
+              width: ScreenUtil().orientation == Orientation.portrait
+                ? 0.2.sh
+                : 0.2.sw,
+              fit: BoxFit.fitWidth,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
